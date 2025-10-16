b425d4944247f92832b88b9976b5a5d786d2fe29
smarchione31 <smarchione@udesa.edu.ar>
2025-10-16 16:19:53 -0300

    funciones para calculo de cada distancia

--- Tp_2.py
+++ Tp_2.py
@@ -5,3 +5,9 @@ import os
 from PIL import Image
 import numpy as np
 import random
+
+def distancia_euclideana(a, b):
+    return (a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2
+
+def distancia_manhattan(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
